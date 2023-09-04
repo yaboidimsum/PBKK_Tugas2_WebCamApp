86d7e206d12e46709e34fac070c90ac67c722512
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-09-04 11:52:05 +0700

    add:Files Webcam

---
+++ WebCamCaptureApp/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WebCamCaptureApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBoxWebcamList = new ComboBox();
+            buttonStart = new Button();
+            buttonCapture = new Button();
+            buttonSaveImage = new Button();
+            buttonExit = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBoxWebcamList
+            // 
+            comboBoxWebcamList.FormattingEnabled = true;
+            comboBoxWebcamList.Location = new Point(12, 22);
+            comboBoxWebcamList.Name = "comboBoxWebcamList";
+            comboBoxWebcamList.Size = new Size(182, 33);
+            comboBoxWebcamList.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(213, 22);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(112, 34);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonCapture
+            // 
+            buttonCapture.Location = new Point(341, 22);
+            buttonCapture.Name = "buttonCapture";
+            buttonCapture.Size = new Size(112, 34);
+            buttonCapture.TabIndex = 2;
+            buttonCapture.Text = "Capture";
+            buttonCapture.UseVisualStyleBackColor = true;
+            buttonCapture.Click += buttonCapture_Click;
+            // 
+            // buttonSaveImage
+            // 
+            buttonSaveImage.Location = new Point(469, 22);
+            buttonSaveImage.Name = "buttonSaveImage";
+            buttonSaveImage.Size = new Size(112, 34);
+            buttonSaveImage.TabIndex = 3;
+            buttonSaveImage.Text = "Save Image";
+            buttonSaveImage.UseVisualStyleBackColor = true;
+            buttonSaveImage.Click += buttonSaveImage_Click;
+            // 
+            // buttonExit
+            // 
+            buttonExit.Location = new Point(1419, 32);
+            buttonExit.Name = "buttonExit";
+            buttonExit.Size = new Size(112, 34);
+            buttonExit.TabIndex = 4;
+            buttonExit.Text = "Exit";
+            buttonExit.UseVisualStyleBackColor = true;
+            buttonExit.Click += buttonExit_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox2, 1, 0);
+            tableLayoutPanel1.Location = new Point(12, 97);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1519, 773);
+            tableLayoutPanel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(753, 767);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.Location = new Point(762, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(754, 767);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1543, 882);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(buttonExit);
+            Controls.Add(buttonSaveImage);
+            Controls.Add(buttonCapture);
+            Controls.Add(buttonStart);
+            Controls.Add(comboBoxWebcamList);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBoxWebcamList;
+        private Button buttonStart;
+        private Button buttonCapture;
+        private Button buttonSaveImage;
+        private Button buttonExit;
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
